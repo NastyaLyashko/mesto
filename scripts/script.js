d2bc6668e0cf9309de34c5ef331218e04bb83503
Nastya Lyashko <lyaschcko.anastasya@yandex.ru>
2020-09-07 22:59:56 +0300

    let переменые

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
 let popup = document.querySelector('.popup');
-const buttonEdit = document.querySelector('.profile__edit-button');
-const formSubmit = document.querySelector('.popup__container');
-const buttonClose = document.querySelector('.popup__close-button');
-const textUserName = document.querySelector('.profile__name');
-const textUserJob = document.querySelector('.profile__job');
-const inputUserName = document.querySelector('.popup__input_type_name');
-const inputUserJob = document.querySelector('.popup__input_type_job');
+let buttonEdit = document.querySelector('.profile__edit-button');
+let formSubmit = document.querySelector('.popup__container');
+let buttonClose = document.querySelector('.popup__close-button');
+let textUserName = document.querySelector('.profile__name');
+let textUserJob = document.querySelector('.profile__job');
+let inputUserName = document.querySelector('.popup__input_type_name');
+let inputUserJob = document.querySelector('.popup__input_type_job');
 
-const loadUserData = () => {
+let loadUserData = () => {
     inputUserName.value = textUserName.textContent;
     inputUserJob.value = textUserJob.textContent;
 }
 
-const saveUserData = () => {
+let saveUserData = () => {
     textUserName.textContent = inputUserName.value;
     textUserJob.textContent = inputUserJob.value;
 }
 
-const openPopup = (event) => {
+let openPopup = (event) => {
     loadUserData();
     popup.classList.add('popup_opened');
 }
 
-const closePopup = (event) => {
+let closePopup = (event) => {
     event.preventDefault();
     popup.classList.remove('popup_opened');
 }
 
-const saveForm = (event) => {
+let saveForm = (event) => {
     saveUserData();
     closePopup(event);
 }
